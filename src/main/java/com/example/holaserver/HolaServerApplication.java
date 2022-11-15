ca99f5d770aaa60f1f3c8cabe68b6b3d277dcdb2
김주원 <juwon5814@gmail.com>
2022-11-15 22:28:59 +0900

    init

---
+++ src/main/java/com/example/holaserver/HolaServerApplication.java
@@ -0,0 +1,14 @@
+package com.example.holaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class}) // db 무시
+public class HolaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/holaserver/HolaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.holaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
