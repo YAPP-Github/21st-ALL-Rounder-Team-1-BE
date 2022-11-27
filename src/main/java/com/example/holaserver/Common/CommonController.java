f8df4ce68ab037dcb990b84d438aed70fc0582ca
김주원 <juwon5814@gmail.com>
2022-11-27 21:23:00 +0900

    ADD ResponseTemplate

--- src/main/java/com/example/holaserver/Common/CommonController.java
+++ src/main/java/com/example/holaserver/Common/CommonController.java
@@ -11,4 +11,5 @@ public class CommonController {
     public boolean heartBeat() {
         return true;
     }
+
 }
---
+++ src/main/java/com/example/holaserver/response/ResponseTemplate.java
@@ -0,0 +1,28 @@
+package com.example.holaserver.response;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"code","message","status"})
+public class ResponseTemplate<T> {
+    private final String message;
+    private final int status;
+    private T data;
+
+    //요청 성공 시
+    public ResponseTemplate(T data, String message){
+        this.message = message;
+        this.status = HttpStatus.OK.value();
+        this.data = data;
+    }
+
+    //요청 실패 시
+    public ResponseTemplate(HttpStatus status, String message){
+        this.message = message + ", ERROR : " + status.getReasonPhrase();
+        this.status = status.value();
+    }
+}
