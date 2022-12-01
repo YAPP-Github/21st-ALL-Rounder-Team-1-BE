c0486350972d5134e39b85ba582a0084c5d70b50
Song-YunMin <thd930308@naver.com>
2022-12-01 21:26:25 +0900

    [Update] local mysql-image 추가

--- src/main/java/com/example/holaserver/Common/CommonController.java
+++ src/main/java/com/example/holaserver/Common/CommonController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommonController {
     @GetMapping("heartbeat")
     public boolean heartBeat() {
-        return true;
+        return false;
     }
-
 }
