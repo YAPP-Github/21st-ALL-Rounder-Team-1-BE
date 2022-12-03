959cdc3aa92590c44162d44e7ca2b69ca07538c2
Song-YunMin <thd930308@naver.com>
2022-12-04 02:33:39 +0900

    [Update] flyway script 완료

---
+++ src/main/resources/db/migration/V1.1__add_cs.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `Customer_Satisfaction` (
+                                         `id` int PRIMARY KEY AUTO_INCREMENT,
+                                         `user_id` int,
+                                         `type` int,
+                                         `content` text
+);
