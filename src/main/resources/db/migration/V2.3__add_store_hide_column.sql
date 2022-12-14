7516517e0df6dc7e2689a51c921c75d43af598e2
김주원 <juwon5814@gmail.com>
2022-12-15 02:25:01 +0900

    뭔가 문제가 발생했다

--- src/main/resources/db/migration/V2.3__add_store_hide_column.sql
+++ src/main/resources/db/migration/V2.3__add_store_hide_column.sql
@@ -1,8 +1,8 @@
-ALTER TABLE `Item` ADD `is_hided` boolean;
-ALTER TABLE `Item` ADD `is_ready` boolean;
+ALTER TABLE `Item` ADD COLUMN `is_hided` boolean;
+ALTER TABLE `Item` ADD COLUMN `is_ready` boolean;
 
-ALTER TABLE `User` ADD `email` varchar(45);
-ALTER TABLE `User` ADD `phone_number` varchar(45);
+ALTER TABLE `User` ADD COLUMN `email` varchar(45);
+ALTER TABLE `User` ADD COLUMN `phone_number` varchar(45);
 
-ALTER TABLE `Store` ADD `notice` text;
-ALTER TABLE `Store` ADD `is_ready` boolean;
+ALTER TABLE `Store` ADD COLUMN `notice` text;
+ALTER TABLE `Store` ADD COLUMN `is_ready` boolean;
