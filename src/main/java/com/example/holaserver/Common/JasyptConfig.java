bee1b8a8bbc9f7fd61382b8afb971ab455fa301c
김주원 <juwon5814@gmail.com>
2022-12-17 18:02:44 +0900

    [Update] update config:

--- src/main/java/com/example/holaserver/Common/JasyptConfig.java
+++ src/main/java/com/example/holaserver/Common/JasyptConfig.java
@@ -1,20 +1,28 @@
 package com.example.holaserver.Common;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.StreamUtils;
 
 import java.io.IOException;
+import java.net.URI;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Configuration
 @EnableEncryptableProperties
+@Slf4j
 public class JasyptConfig {
     @Bean("jasyptStringEncryptor")
     public StringEncryptor stringEncryptor() {
@@ -34,9 +42,8 @@ public class JasyptConfig {
 
     private String getJasyptEncryptorPassword() {
         try {
-            ClassPathResource resource = new ClassPathResource("db/jasypt-encryptor-password.txt");
-            return Files.readAllLines(Paths.get(resource.getURI())).stream()
-                    .collect(Collectors.joining(""));
+        ClassPathResource resource = new ClassPathResource("secret/rds-dev-secret-key.txt");
+        return resource.getInputStream().readAllBytes().toString();
         } catch (IOException e) {
             throw new RuntimeException("Not found Jasypt password file.");
         }
