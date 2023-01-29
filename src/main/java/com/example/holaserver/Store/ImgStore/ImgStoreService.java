56e1366a326e2f1d2e9bf557eb2e7499cb0af5b3
Song-YunMin <thd930308@naver.com>
2023-01-29 18:26:14 +0900

    [Update] 주석 제거

--- src/main/java/com/example/holaserver/Store/ImgStore/ImgStoreService.java
+++ src/main/java/com/example/holaserver/Store/ImgStore/ImgStoreService.java
@@ -15,13 +15,6 @@ public class ImgStoreService {
     private final ImgStoreRepository imgStoreRepository;
 
     public List<Long> saveImgStores(Long storeId, String[] pathDatas) {
-//        List<Long> imgStoreIds = new ArrayList<Long>();
-//        pathDatas = pathDatas.replaceAll("[\\[\\[\\]]","");
-//        if (pathDatas.equals("")) return imgStoreIds;
-//        for (String path : pathDatas.split(",")) {
-//            ImgStore imgStore = new ImgStoreBody(storeId, path).createSaveImgStoreBuilder();
-//            imgStoreIds.add(this.imgStoreRepository.save(imgStore).getId());
-//        }
         return Arrays.stream(pathDatas).map(pathData -> {
             ImgStore imgStore = new ImgStoreBody(storeId, pathData).createSaveImgStoreBuilder();
             return this.imgStoreRepository.save(imgStore).getId();
