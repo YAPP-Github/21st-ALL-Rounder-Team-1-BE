45b7af6d467e1f9a62e5909178f8546490b969a4
Song-YunMin <thd930308@naver.com>
2023-01-30 01:39:42 +0900

    [Update] find review 로직 추가

--- src/main/java/com/example/holaserver/Review/ReviewService.java
+++ src/main/java/com/example/holaserver/Review/ReviewService.java
@@ -40,6 +40,7 @@ public class ReviewService {
     }
 
     public List<ReviewByStoreResponse> findReviewAndRelationInfo(Long storeId) {
+        List<Review> reviews = reviewRepository.findReviewsByStoreId(storeId);
 
     }
 
