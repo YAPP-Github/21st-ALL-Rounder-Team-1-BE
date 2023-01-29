a3383346d86664f2d3d880ad83d96164e02bd2c6
Song-YunMin <thd930308@naver.com>
2023-01-30 01:46:34 +0900

    [Update] 리뷰 by 가게 response 정의

--- src/main/java/com/example/holaserver/Review/DTO/ReviewByStoreResponse.java
+++ src/main/java/com/example/holaserver/Review/DTO/ReviewByStoreResponse.java
@@ -1,20 +1,35 @@
 package com.example.holaserver.Review.DTO;
 
 import com.example.holaserver.Review.ImgReview.ImgReview;
+import com.example.holaserver.Review.Review;
 import com.example.holaserver.Review.ReviewTagLog.ReviewTagLog;
 import com.example.holaserver.User.User;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class ReviewByStoreResponse {
-    private Long id;
-    private Long storeId;
-    private Long userId;
-    private String reviewText;
-    private String createdAt;
-    private String modifiedAt;
-    private User user;
-    private ImgReview[] imgReviews;
-    private ReviewTagLog[] reviewTagLogs;
+    private final Long id;
+    private final Long storeId;
+    private final Long userId;
+    private final String reviewText;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime modifiedAt;
+    private final User user;
+    private final ImgReview[] imgReviews;
+    private final ReviewTagLog[] reviewTagLogs;
+
+    public ReviewByStoreResponse(Review review, User user, ImgReview[] imgReviews, ReviewTagLog[] reviewTagLogs) {
+        this.id = review.getId();
+        this.storeId = review.getStoreId();
+        this.userId = review.getUserId();
+        this.reviewText = review.getReviewText();
+        this.createdAt = review.getCreatedAt();
+        this.modifiedAt = review.getModifiedAt();
+        this.user = user;
+        this.imgReviews = imgReviews;
+        this.reviewTagLogs = reviewTagLogs;
+    }
 
 }
