a2cd44efcafc35bb12cad2634458ad17f1291b91
김주원 <juwon5814@gmail.com>
2023-02-06 13:17:30 +0900

    [Add] 닉네임 생성 API

--- src/main/java/com/example/holaserver/User/UserRepository.java
+++ src/main/java/com/example/holaserver/User/UserRepository.java
@@ -8,4 +8,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByOauthIdentityAndOauthType(String oauthIdentity, String oauthType);
     User findByOauthIdentity(String oauthIdentity);
     Boolean existsByNickname(String nickname);
+    int countUserByNicknameContaining(String nickname);
 }
